8f59bb76bb5848efe983848f2caf52befd228378
Angelo Breuer <angelobr@outlook.de>
2022-11-13 21:20:49 +0100

    Initial Commit

---
+++ NerdctlWsl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+var processStartInfo = new ProcessStartInfo("wsl.exe");
+
+processStartInfo.ArgumentList.Add("/usr/bin/nerdctl");
+
+foreach (var argument in args)
+{
+    var mappedArgument = argument;
+
+    if (File.Exists(mappedArgument))
+    {
+        mappedArgument = Path.GetFullPath(mappedArgument);
+
+        var originalFileRoot = Path.GetPathRoot(mappedArgument)!;
+        var root = originalFileRoot.TrimEnd('\\').TrimEnd(':').ToLowerInvariant();
+        var relativePath = mappedArgument[originalFileRoot.Length..].Trim('/').Trim('\\');
+
+        mappedArgument = $"/mnt/{root}/{relativePath.Replace('\\', '/')}";
+    }
+
+    processStartInfo.ArgumentList.Add(mappedArgument);
+}
+
+Process.Start(processStartInfo)!.WaitForExit();
